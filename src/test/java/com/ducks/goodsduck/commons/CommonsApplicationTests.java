15c5901b5390926a45a1aafebc6d34ee5d7fd04c
Ting-Kim <taeho916@gmail.com>
2021-07-12 18:58:26 +0900

    [chore] 배포 빌드에서 실패하는 테스트 코드 삭제

--- src/test/java/com/ducks/goodsduck/commons/CommonsApplicationTests.java
+++ src/test/java/com/ducks/goodsduck/commons/CommonsApplicationTests.java
@@ -1,12 +1,7 @@
 package com.ducks.goodsduck.commons;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.config.location=" +
-		"classpath:/application.yml," +
-		"classpath:/application-db.yml," +
-		"classpath:/application-oauth2.yml")
 class CommonsApplicationTests {
 
 	@Test
