b1e3b060b9026940c2c05ca250d9d5a5ae9cd329
Ting-Kim <taeho916@gmail.com>
2021-07-14 12:57:24 +0900

    [build] Spring Cloud 및 Aws Secrets Manager 테스트

---
+++ src/main/java/com/ducks/goodsduck/commons/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.ducks.goodsduck.commons.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/test")
+public class TestController {
+
+    @Value(value = "${spring.datasource.username}")
+    private String dbUsername;
+
+    @GetMapping("/v1")
+    public Map<String, Object> getProperties(HttpServletRequest request) {
+        final Map<String, Object> map = new HashMap<>();
+        map.put("DBUsername", dbUsername);
+        return map;
+    }
+}
