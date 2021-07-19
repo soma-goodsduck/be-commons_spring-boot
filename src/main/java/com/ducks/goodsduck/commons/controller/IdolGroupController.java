c6bcfdc14b069964751bc058b731864d7f879ffe
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2021-07-19 10:44:33 +0900

    [refactor] IdolGroup 컨트롤러 리팩토링

--- src/main/java/com/ducks/goodsduck/commons/controller/IdolGroupController.java
+++ src/main/java/com/ducks/goodsduck/commons/controller/IdolGroupController.java
@@ -1,7 +1,10 @@
 package com.ducks.goodsduck.commons.controller;
 
+import com.ducks.goodsduck.commons.annotation.NoCheckJwt;
 import com.ducks.goodsduck.commons.model.dto.idol.IdolGroupDto;
 import com.ducks.goodsduck.commons.service.IdolGroupService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +17,14 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Api(tags = "아이돌 그룹 APIs")
 public class IdolGroupController {
 
     private final IdolGroupService idolGroupService;
 
-    /** 아이돌 그룹 리스트 가져오기 API */
+    @NoCheckJwt
     @GetMapping("/idol")
+    @ApiOperation("아이돌 그룹 리스트 가져오기 API")
     public List<IdolGroupDto> getIdolGroups() {
         return idolGroupService.getIdolGroups()
                 .stream()
@@ -27,8 +32,9 @@ public class IdolGroupController {
                 .collect(Collectors.toList());
     }
 
-    /** 아이돌 그룹 가져오기 API */
+    @NoCheckJwt
     @GetMapping("/idol/{idol_group_id}")
+    @ApiOperation("아이돌 그룹 가져오기 API")
     public IdolGroupDto getIdolGroup(@PathVariable("idol_group_id") Long idolGroupId) {
         return idolGroupService.getIdolGroup(idolGroupId)
                 .map(idolGroup -> new IdolGroupDto(idolGroup))
