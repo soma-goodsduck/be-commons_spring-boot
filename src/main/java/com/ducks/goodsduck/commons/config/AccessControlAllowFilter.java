210af4ce35d42d31a1acfef11de7790d65075392
Ting-Kim <taeho916@gmail.com>
2021-07-19 16:20:05 +0900

    [chore] Access-Origin-Exposed-Headers 문제 해결

--- src/main/java/com/ducks/goodsduck/commons/config/AccessControlAllowFilter.java
+++ src/main/java/com/ducks/goodsduck/commons/config/AccessControlAllowFilter.java
@@ -18,7 +18,7 @@ public class AccessControlAllowFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Origin-Allow-Headers", "jwt");
+        response.setHeader("Access-Control-Expose-Headers", "jwt");
 
         if(request.getMethod().equals(HttpMethod.OPTIONS.name())){
             response.setStatus(HttpStatus.OK.value());
