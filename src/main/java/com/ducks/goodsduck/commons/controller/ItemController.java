1c39cca36e7a8507edc95039e5130585e831d81e
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2021-07-23 19:52:45 +0900

    [chore] 테스트용 고정값 제거

--- src/main/java/com/ducks/goodsduck/commons/controller/ItemController.java
+++ src/main/java/com/ducks/goodsduck/commons/controller/ItemController.java
@@ -116,7 +116,6 @@ public class ItemController {
                                                                        @RequestParam("pageNumber") Integer pageNumber) {
 
         Long userId = userService.checkLoginStatus(jwt);
-        userId = 8L;
 
         // HINT : 비회원에게 보여줄 홈
         if(userId.equals(-1L)) {
