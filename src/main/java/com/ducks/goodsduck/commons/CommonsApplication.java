97a479ae18f177a2a675063d0f4961f9dc466ee9
Ting-Kim <taeho916@gmail.com>
2021-07-23 14:04:17 +0900

    [chore] 로드밸런서를 통한 https 도입 및 도메인 연결로 인해 Swagger 버전(2->3) 변경

--- src/main/java/com/ducks/goodsduck/commons/CommonsApplication.java
+++ src/main/java/com/ducks/goodsduck/commons/CommonsApplication.java
@@ -3,13 +3,13 @@ package com.ducks.goodsduck.commons;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class CommonsApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CommonsApplication.class, args);
 	}
 }
