a2c3483085dfa639a377860fe267b35afeaf938f
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-02 09:59:38 +0900

    [fix] CORS 해결 체크

---
+++ src/main/java/com/ducks/goodsduck/commons/model/dto/UserIdolGroupUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.ducks.goodsduck.commons.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserIdolGroupRequest {
+
+//    List<String>
+}
