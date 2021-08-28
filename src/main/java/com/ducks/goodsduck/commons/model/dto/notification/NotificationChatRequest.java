dcea3cbee592d0a1cf57abe05b3fa6bcc5deaced
Ting-Kim <taeho916@gmail.com>
2021-08-29 00:10:59 +0900

    [chore] NotificationRequest -> NotificationChatRequest 클래스명 변경

--- src/main/java/com/ducks/goodsduck/commons/model/dto/notification/NotificationChatRequest.java
+++ src/main/java/com/ducks/goodsduck/commons/model/dto/notification/NotificationChatRequest.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class NotificationRequest {
+public class NotificationChatRequest {
 
     private String chatRoomId;
     private Long senderId;
     private NotificationType type;
 
-    public NotificationRequest(String chatRoomId, Long senderId, NotificationType type) {
+    public NotificationChatRequest(String chatRoomId, Long senderId, NotificationType type) {
         this.chatRoomId = chatRoomId;
         this.senderId = senderId;
         this.type = type;
