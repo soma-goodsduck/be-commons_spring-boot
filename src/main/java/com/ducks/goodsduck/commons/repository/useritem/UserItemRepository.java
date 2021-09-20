fecb8092c7ec72b586e1e8a4b0f31dddb313249a
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-09-21 00:14:25 +0900

    [chore] 아이템 삭제에 따른 찜 굿즈 처리

--- src/main/java/com/ducks/goodsduck/commons/repository/useritem/UserItemRepository.java
+++ src/main/java/com/ducks/goodsduck/commons/repository/useritem/UserItemRepository.java
@@ -2,9 +2,13 @@ package com.ducks.goodsduck.commons.repository.useritem;
 
 import com.ducks.goodsduck.commons.model.entity.UserItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
-    Long countByUserId(Long userId);
+
+    @Query("select count(ui) from UserItem ui where ui.user.id = :userId and ui.item.deletedAt is null")
+    Long countByUserId(@Param("userId") Long userId);
 }
