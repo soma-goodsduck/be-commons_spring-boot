5c8323aec1aa5bb1680c1abe3a2d9b1ad3d0906e
Ting-Kim <taeho916@gmail.com>
2021-11-02 17:55:49 +0900

    [fix] null 체크 및 자료형 오류 해결

--- src/main/java/com/ducks/goodsduck/commons/annotation/IntegerArrayConverter.java
+++ src/main/java/com/ducks/goodsduck/commons/annotation/IntegerArrayConverter.java
@@ -8,20 +8,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Converter
-public class IntegerArrayConverter implements AttributeConverter<List<Integer>, String> {
+public class IntegerArrayConverter implements AttributeConverter<List<Long>, String> {
 
     private static final String SPLIT_CHAR = ",";
 
     @Override
-    public String convertToDatabaseColumn(List<Integer> attribute) {
+    public String convertToDatabaseColumn(List<Long> attribute) {
         if (attribute == null) return "";
         return attribute.stream().map(String::valueOf).collect(Collectors.joining(SPLIT_CHAR));
     }
 
     @Override
-    public List<Integer> convertToEntityAttribute(String dbData) {
+    public List<Long> convertToEntityAttribute(String dbData) {
+        if (dbData == null) return new ArrayList<>();
         return Arrays.stream(dbData.split(SPLIT_CHAR))
-                .map(Integer::parseInt)
+                .map(Long::parseLong)
                 .collect(Collectors.toList());
 
     }
